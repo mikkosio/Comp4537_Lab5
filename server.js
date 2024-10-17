5ca93e4bac7736fec226dbbf858c49ecd58dc130
Mikko Sio <msio1@my.bcit.ca>
2024-10-17 03:14:36 -0700

    test3

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ function handlePost(req, res) {
 }
 
 async function handleRequest(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     await sqlHandler.createTable();
